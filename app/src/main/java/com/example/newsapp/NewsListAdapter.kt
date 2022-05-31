28c30345a6cabfab5dea93f52a80c384f30f401d
Ansh <anshkevadiya1802@gmail.com>
2022-05-31 16:21:36 +0530

    completed application with Api and stuff

--- app/src/main/java/com/example/newsapp/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.kt
@@ -3,41 +3,49 @@ package com.example.newsapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class NewsListAdapter(val items :ArrayList<String>,private val listner:NewItemclicked): RecyclerView.Adapter<NewsViewHolder>() {
+class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val items: ArrayList<News> = ArrayList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
-        val viewHolder=NewsViewHolder(view)
-        view.setOnClickListener {
-            listner.onitemclicked(items[viewHolder.adapterPosition])
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        val viewHolder = NewsViewHolder(view)
+        view.setOnClickListener{
+            listener.onItemClicked(items[viewHolder.adapterPosition])
         }
-
-
         return viewHolder
-    }
-
-    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val currentItem=items[position]
-        holder.titleview.text=currentItem
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem.title
+        holder.author.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.Imageurl).into(holder.image)
+    }
+
+    fun updateNews(updatedNews: ArrayList<News>) {
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
+    }
 }
 
-
-
-class NewsViewHolder(itemview: View):RecyclerView.ViewHolder(itemview)
-{
-    val titleview:TextView=itemview.findViewById(R.id.title)
-
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val titleView: TextView = itemView.findViewById(R.id.title)
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
 }
 
-interface NewItemclicked
-{
-    fun onitemclicked(item:String)
+interface NewsItemClicked {
+    fun onItemClicked(item: News)
 }
---
+++ app/src/main/java/com/example/newsapp/splash_screen.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+
+    }
+}
